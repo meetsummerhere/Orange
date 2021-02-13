c67c144ff4c7638801e33042a43a8bd5008eb1af
meetsummerhere <rosita260796@gmail.com>
2021-02-13 07:48:02 +0700

    up

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Server.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+/**
+ * Created by KUNCORO on 24/03/2017.
+ */
+
+public class Server {
+    public static final String URL = "http://192.168.42.65/android/api/";
+}
